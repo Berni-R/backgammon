257e1b59cfda996f29abe20d705346f9578db261
Bernhard Röttgers <bernhard.roettgers@yahoo.de>
2022-11-15 21:16:30 +0100

    move Action in own file

--- backgammon/moves/__init__.py
+++ backgammon/moves/__init__.py
@@ -4,8 +4,8 @@ from . import legal_actions
 
 from .move import Move
 from .move_legal import assert_legal_move, is_legal_move, build_legal_move, build_legal_moves
+from .action import Action
 from .legal_actions import (
-    Action,
     assert_legal_action, is_legal_action,
     do_action, undo_action, build_legal_actions
 )
--- backgammon/moves/legal_actions.py
+++ backgammon/moves/legal_actions.py
@@ -1,90 +1,11 @@
-from typing import Any, Sequence, Optional, Iterator, Union
+from typing import Sequence, Optional
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
 
 from ..core import Color, IllegalMoveError
-from .move import Move
 from ..board import Board
 from .move_legal import assert_legal_move, build_legal_move
-
-
-class Action:
-
-    def __init__(self, moves: Sequence[Move] = (), doubles: int = 0, takes: Optional[bool] = None):
-        self.moves = [] if doubles else [m for m in moves]
-        self.doubles = doubles
-        self.takes = takes
-
-    def __copy__(self) -> 'Action':
-        return Action(
-            [move for move in self.moves],
-            self.doubles,
-            self.takes,
-        )
-
-    def copy(self) -> 'Action':
-        return self.__copy__()
-
-    def __eq__(self, other: Any) -> bool:
-        return isinstance(other, Action) and (
-            # set(self.moves) == set(other.moves)
-            len(self.moves) == len(other.moves)
-            and all(a == b for a, b in zip(self.moves, other.moves))
-            and self.doubles == other.doubles
-            and self.takes == other.takes
-        )
-
-    def __hash__(self) -> int:
-        return hash(tuple(self.moves) + (self.doubles, self.takes))
-
-    @property
-    def is_dropping(self) -> bool:
-        return self.takes is not None and not self.takes
-
-    @property
-    def dances(self) -> bool:
-        return len(self.moves) == 0 and (not self.doubles)
-
-    def __repr__(self) -> str:
-        r = f"Action({self.moves}"
-        if self.doubles:
-            r += f", doubles={self.doubles}"
-        if self.takes is not None:
-            r += f", takes={self.takes}"
-        return r + ")"
-
-    def to_str(self, bar_off: bool = True, regular: bool = True) -> str:
-        if self.takes is not None:
-            return 'Takes' if self.takes else 'Drops'
-        if self.doubles:
-            return f'Doubles => {self.doubles}'
-        if len(self.moves) == 0:
-            return 'Dances'
-        return ' '.join(move.to_str(bar_off, regular) for move in self.moves)
-
-    def __str__(self) -> str:
-        return self.to_str()
-
-    def __iter__(self) -> Iterator[Move]:
-        return iter(self.moves)
-
-    def __getitem__(self, item: int | slice) -> Union[Move, 'Action']:
-        if isinstance(item, int):
-            return self.moves[item]
-        elif isinstance(item, slice):
-            return Action(self.moves[item], self.doubles, self.takes)
-        else:
-            raise TypeError(f"cannot subscript Action with type {type(item)}")
-
-    def __add__(self, other: 'Action') -> 'Action':
-        if self.doubles != 0 or other.doubles:
-            raise ValueError("cannot add to doubling actions")
-        return Action(self.moves + other.moves)
-
-    def __radd__(self, moves: list[Move]) -> 'Action':
-        if self.doubles != 0:
-            raise ValueError("cannot (right) add to doubling action")
-        return Action(moves + self.moves)
+from .action import Action
 
 
 def assert_legal_action(action: Action, board: Board, pseudolegal: bool = False):
